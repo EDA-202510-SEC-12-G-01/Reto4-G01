34bd7f992260bf2b970bfb10dbb3316b84125b45
Camilo Eduardo Castro Torres <c.castrot@uniandes.edu.co>
2025-05-27 11:29:01 -0500

    algunos cmabios

--- DataStructures/Graph/dijkstra.py
+++ DataStructures/Graph/dijkstra.py
@@ -1,37 +1,48 @@
 from DataStructures.Graph import edge as ed
 from DataStructures.Graph import vertex as vt
-from DataStructures.Graph import diagraph as G
+from DataStructures.Graph import digraph as G
 
 from DataStructures.List import array_list as al
+from DataStructures.List.list_iterator import iterator
 from DataStructures.Map import map_linear_probing as mlp
-from DataStructures.Priority_queue import priority_queue as pq
+from DataStructures.Priority_queue import index_priority_queue as ipq
 from DataStructures.Stack import stack as s
 
 def dijkstra(my_graph, source):
     if not G.contains_vertex(my_graph, source):
         raise Exception("El vertice source no existe")
+    
     dist_to = mlp.new_map(G.order(my_graph), 0.7)
     edge_to = mlp.new_map(G.order(my_graph), 0.7)
     marked = mlp.new_map(G.order(my_graph), 0.7)
-    ipq = pq.new_heap()
-    vertices_list = G.vertices(my_graph)
-    for i in range(al.size(vertices_list)):
-        v = al.get_element(vertices_list, i)
+    pq = ipq.new_index_heap(True) 
+
+
+    for v in iterator(G.vertices(my_graph)):
         mlp.put(dist_to, v, float('inf'))
+    
+
     mlp.put(dist_to, source, 0.0)
-    pq.insert(ipq, source, 0.0)
-    while not pq.is_empty(ipq):
-        v = pq.remove(ipq)
+    ipq.insert(pq, 0.0, source) 
+    while not ipq.is_empty(pq):
+        v = ipq.remove(pq)
         mlp.put(marked, v, True)
-        adjacents_list = G.adjacents(my_graph, v)
-        for i in range(al.size(adjacents_list)):
-            w = al.get_element(adjacents_list, i)
+        
+
+        for w in iterator(G.adjacents(my_graph, v)):
             e = G.get_edge(my_graph, v, w)
             wt = ed.weight(e)
+            
             if mlp.get(dist_to, v) + wt < mlp.get(dist_to, w):
-                mlp.put(dist_to, w, mlp.get(dist_to, v) + wt)
+                new_dist = mlp.get(dist_to, v) + wt
+                mlp.put(dist_to, w, new_dist)
                 mlp.put(edge_to, w, v)
-                pq.insert(ipq, w, mlp.get(dist_to, w))
+                
+                if ipq.contains(pq, w):
+                    ipq.decrease_key(pq, w, new_dist)
+                else:
+                    ipq.insert(pq, new_dist, w)
+    
     return {'dist_to': dist_to, 'edge_to': edge_to, 'marked': marked}
 
 def dist_to(key_v, dijkstra_search):
@@ -46,11 +57,14 @@ def has_path_to(key_v, dijkstra_search):
 def path_to(key_v, dijkstra_search):
     if not has_path_to(key_v, dijkstra_search):
         return None
+    
     path = s.new_stack()
     v = key_v
     edge_to_map = dijkstra_search['edge_to']
+    
     while mlp.contains(edge_to_map, v) and mlp.get(edge_to_map, v) is not None:
         s.push(path, v)
         v = mlp.get(edge_to_map, v)
     s.push(path, v)
+    
     return path
--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -1,241 +1,123 @@
 from DataStructures.List import array_list as lt
 
-
 def new_heap(is_min_pq=True):
     """
-    Crea un cola de prioridad indexada orientada a menor o mayor dependiendo del valor de ``is_min_pq``
-
-    Se crea una cola de prioridad con los siguientes atributos:
-
-    - **elements**: Lista de elementos. Se inicializa como una lista vacia.
-    - **cmp_function**: La funcion de comparacion. Si es una cola de prioridad orientada a menor ``is_min_pq = True``, se inicializa con la funcion ``cmp_function_lower_value``. Si es una cola de prioridad orientada a mayor ``is_min_pq = False``, se inicializa con la funcion ``cmp_function_higher_value``.
-
-    :param is_min_pq: Indica si la cola de prioridad es orientada a menor o mayor. Por defecto es ``True``.
-    :type is_min_pq: bool
-
-    :return: Una nueva cola de prioridad indexada
-    :rtype: priority_queue
+    Crea un nuevo heap (cola de prioridad)
     """
-    heap = {"elements": lt.new_list(), "size": 0, "cmp_function": None}
-    lt.add_last(heap["elements"], None)
-    if is_min_pq:
-        heap["cmp_function"] = cmp_function_lower_value
-    else:
-        heap["cmp_function"] = cmp_function_higher_value
+    heap = {
+        'elements': lt.new_list(),
+        'size': 0,
+        'is_min_heap': is_min_pq
+    }
     return heap
-
-
-def cmp_function_higher_value(father_node, child_node):
-    """
-    Valida si el nodo padre tiene mayor prioridad que el nodo hijo
-
-    :param father_node: El nodo padre
-    :type father_node: dict
-    :param child_node: El nodo hijo
-    :type child_node: dict
-
-    :return: ``True`` si el nodo padre tiene mayor prioridad que el nodo hijo. ``False`` en caso contrario.
-    :rtype: bool
-    """
-    if father_node["key"] >= child_node["key"]:
-        return True
-    return False
-
-
-def cmp_function_lower_value(father_node, child_node):
-    """
-    Valida si el nodo padre tiene menor prioridad que el nodo hijo
-
-    :param father_node: El nodo padre
-    :type father_node: dict
-    :param child_node: El nodo hijo
-    :type child_node: dict
-
-    :return: ``True`` si el nodo padre tiene menor prioridad que el nodo hijo. ``False`` en caso contrario.
-    :rtype: bool
-    """
-    if father_node["key"] <= child_node["key"]:
-        return True
-    return False
-
 
 def size(my_heap):
     """
-    Retorna el número de elementos en el heap.
-
-    :param my_heap: El heap a revisar
-    :type my_heap: priority_queue
-
-    :return: El número de elementos
-    :rtype: int
+    Retorna el tamaño del heap
     """
-    return my_heap["size"]
-
+    return my_heap['size']
 
 def is_empty(my_heap):
     """
-    Informa si una cola de prioridad es vacia.
-
-    :param my_heap: El heap indexado a revisar
-    :type my_heap: priority_queue
-
-    :return: ``True`` si esta vacia. ``False`` en caso contrario.
-    :rtype: bool
+    Verifica si el heap está vacío
     """
-    return my_heap["size"] == 0
-
-
-def get_first_priority(my_heap):
-    """
-    Retorna el primer elemento del heap, es decir el elemento con mayor prioridad sin eliminarlo.
-
-    .. important:: Si el heap es orientado a menor, el primer elemento es el de menor valor. Si el heap es orientado a mayor,
-    el primer elemento es el de mayor valor.
-
-    :param my_heap: El heap a revisar
-    :type my_heap: priority_queue
-
-    :return: El valor asociada al mayor indice
-    :rtype: any
-    """
-    if my_heap["size"] > 0:
-        return lt.get_element(my_heap["elements"], 1)["value"]
-    return None
-
-
-def insert(my_heap, element, key):
-    """
-    Inserta la llave ``key`` con prioridad ``value`` en el heap.
-
-    :param my_heap: El heap indexado
-    :type my_heap: priority_queue
-    :param element: El valor de la llave
-    :type element: int
-    :param key: La llave a insertar
-    :type key: any
-
-    :return: El heap con la nueva paraja insertada
-    :rtype: priority_queue
-    """
-    my_heap["size"] += 1
-    lt.insert_element(my_heap["elements"], {
-                      "key": key, "value": element}, my_heap["size"])
-    swim(my_heap, my_heap["size"])
-    return my_heap
-
-
-def remove(my_heap):
-    """
-    Retorna el elemento del heap de mayor prioridad y lo elimina.
-    Se reemplaza con el último elemento y se hace **sink**.
-
-    .. important:: Si el heap es orientado a menor, el primer elemento es el de menor valor.
-        Si el heap es orientado a mayor, el primer elemento es el de mayor valor.
-
-    :param my_heap: El heap a revisar
-    :type my_heap: priority_queue
-
-    :return: Valor asociado a la key con mayor prioridad
-    :rtype: any
-    """
-    if my_heap["size"] > 0:
-        min = lt.get_element(my_heap["elements"], 1)
-        last = lt.get_element(my_heap["elements"], my_heap["size"])
-        lt.change_info(my_heap["elements"], 1, last)
-        lt.remove_last(my_heap["elements"])
-        my_heap["size"] -= 1
-        sink(my_heap, 1)
-        return min["value"]
-    return None
-
-
-# _____________________________________________________________________________
-#       Funciones Helper
-# _____________________________________________________________________________
-
+    return my_heap['size'] == 0
 
 def swim(my_heap, pos):
     """
-    Deja en el lugar indicado un elemento adicionado en la última posición
-
-    :param my_heap: El heap sobre el cual se realiza la operación
-    :type my_heap: priority_queue
-    :param pos: La posición del elemento a revisar
-    :type pos: int
+    Reestablecer propiedad del heap hacia arriba
     """
-    found = False
-    while pos > 1 and not found:
-        parent = lt.get_element(my_heap["elements"], int((pos // 2)))
-        element = lt.get_element(my_heap["elements"], int(pos))
-        if not priority(my_heap, parent, element):
-            exchange(my_heap, pos, int(pos / 2))
+    while pos > 1:
+        parent_pos = pos // 2
+        parent = lt.get_element(my_heap["elements"], parent_pos - 1)
+        child = lt.get_element(my_heap["elements"], pos - 1)
+        
+        # Para min-heap: hijo < padre, para max-heap: hijo > padre
+        should_swap = False
+        if my_heap['is_min_heap']:
+            should_swap = child['key'] < parent['key']
         else:
-            found = True
-        pos = pos // 2
-
+            should_swap = child['key'] > parent['key']
+        
+        if should_swap:
+            lt.exchange(my_heap["elements"], pos - 1, parent_pos - 1)
+            pos = parent_pos
+        else:
+            break
+    return my_heap
 
 def sink(my_heap, pos):
     """
-    Deja en la posición correcta un elemento ubicado en la raíz del heap
-
-    :param my_heap: El heap sobre el cual se realiza la operación
-    :type my_heap: priority_queue
-    :param pos: La posición del elemento a revisar
-    :type pos: int
+    Reestablecer propiedad del heap hacia abajo
     """
-    size = my_heap["size"]
-    while 2 * pos <= size:
-        j = 2 * pos
-        if j < size:
-            if not priority(
-                my_heap,
-                lt.get_element(my_heap["elements"], j),
-                lt.get_element(my_heap["elements"], (j + 1)),
-            ):
-                j += 1
-        if priority(
-            my_heap,
-            lt.get_element(my_heap["elements"], pos),
-            lt.get_element(my_heap["elements"], j),
-        ):
+    elements_list = my_heap['elements']
+    heap_size = my_heap['size']
+    
+    while 2 * pos <= heap_size:
+        left = 2 * pos
+        right = left + 1
+        extremo = pos
+        
+        current_key = lt.get_element(elements_list, extremo - 1)['key']
+        
+        if left <= heap_size:
+            left_key = lt.get_element(elements_list, left - 1)['key']
+            if my_heap['is_min_heap']:
+                if left_key < current_key:
+                    extremo = left
+                    current_key = left_key
+            else:
+                if left_key > current_key:
+                    extremo = left
+                    current_key = left_key
+        
+        if right <= heap_size:
+            right_key = lt.get_element(elements_list, right - 1)['key']
+            if my_heap['is_min_heap']:
+                if right_key < current_key:
+                    extremo = right
+            else:
+                if right_key > current_key:
+                    extremo = right
+        
+        if extremo == pos:
             break
-        exchange(my_heap, pos, j)
-        pos = j
+        
+        lt.exchange(elements_list, pos - 1, extremo - 1)
+        pos = extremo
 
-
-def priority(my_heap, parent, child):
+def insert(my_heap, value, key):
     """
-    Indica si el ``parent`` tiene mayor prioridad que ``child``.
-
-    .. important:: La prioridad se define por la función de comparación del heap. Si es un heap orientado a menor,
-        la prioridad es menor si el ``parent`` es menor que el ``child``. Si es un heap orientado a mayor, la prioridad es mayor
-        si el ``parent`` es mayor que el ``child``.
-
-    :param my_heap: El heap a revisar
-    :type my_heap: priority_queue
-    :param parent: El elemento padre
-    :type parent: any
-    :param child: El elemento hijo a comparar
-    :type child: any
-
-    :returns: ``True`` si el ``parent`` tiene mayor prioridad que el ``child``. ``False`` en caso contrario.
-    :rtype: bool
+    Inserta un nuevo elemento en el heap
     """
-    cmp = my_heap["cmp_function"](parent, child)
-    if cmp > 0:
-        return True
-    return False
+    my_heap['size'] += 1
+    lt.add_last(my_heap['elements'], {'key': key, 'value': value})
+    swim(my_heap, my_heap['size'])
 
-
-def exchange(my_heap, pos_i, pos_j):
+def get_first_priority(my_heap):
     """
-    Intercambia los elementos en las posiciones ``pos_i`` y ``pos_j`` del heap
-
-    :param my_heap: El heap a revisar
-    :type my_heap: priority_queue
-    :param pos_i: La posición del primer elemento
-    :type pos_i: int
-    :param pos_j: La posición del segundo elemento
-    :type pos_j: int
+    Retorna el elemento con mayor prioridad sin removerlo
     """
-    lt.exchange(my_heap["elements"], pos_i, pos_j)
+    if my_heap['size'] == 0:
+        return None
+    return lt.first_element(my_heap["elements"])['value']
+
+def remove(my_heap):
+    """
+    Remueve y retorna el elemento con mayor prioridad
+    """
+    if my_heap['size'] == 0:
+        return None
+    
+    # Obtener el elemento con mayor prioridad
+    max_priority_element = lt.get_element(my_heap['elements'], 0)
+    
+    # Mover el último elemento al principio
+    last_element = lt.remove_last(my_heap['elements'])
+    my_heap['size'] -= 1
+    
+    if my_heap['size'] > 0:
+        lt.change_info(my_heap['elements'], 0, last_element)
+        sink(my_heap, 1)
+    
+    return max_priority_element['value']
